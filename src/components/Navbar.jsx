e5c845bb28d74295d78c57a4a956a336c0db55d8
Krpardo27 <kevin.pardov26@gmail.com>
2025-04-30 23:36:47 -0400

    se ajustó fontweight

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const NavBar = () => {
             <Link
               to="/contact"
               onClick={scrollToTop}
-              className={`flex items-center relative px-3 py-2 rounded-md text-sm font-medium group
+              className={`flex items-center relative px-1 py-2 transition-colors group
                 ${
                   isActive("/contact")
                     ? "text-blue-600"
