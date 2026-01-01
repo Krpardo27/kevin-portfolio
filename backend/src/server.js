3e67cd8734aa76c6643a98a0ccb8eed47bbbbb10
Krpardo27 <kevin.pardov26@gmail.com>
2025-12-31 21:42:05 -0300

    Se pone api en las peticiones

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/", contactLimiter, contactRoutes);
+app.use("/api", contactLimiter, contactRoutes);
 
 const PORT = process.env.PORT || 5000;
 
